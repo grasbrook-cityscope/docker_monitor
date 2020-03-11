270f954c507bfcb8764d36ecfa5e5c999b6c77a9
Jonas <jonas.luft@hcu-hamburg.de>
2020-03-11 14:15:09 +0100

    trim docekr output

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ fi
 
 webhook=$1
 
-exited_containers=$(docker ps -a | grep Exited)
+exited_containers=$(docker ps -a | grep Exited | sed "s/^.*Exited/Exited/")
 if [ -n "$exited_containers" ]; 
 then 
     echo "found exited container!"
-    python send_to_mattermost.py $webhook $exited_containers
+    python3 send_to_mattermost.py $webhook $exited_containers
 fi
