963c19072205cf956728173a9ee962a90bc3409f
Jonas <jonas.luft@hcu-hamburg.de>
2020-03-11 14:17:39 +0100

    activate post

--- send_to_mattermost.py
+++ send_to_mattermost.py
@@ -5,15 +5,14 @@ def send(url, data):
     # wrap neatly in json
     json_payload ="{ \'text\': \'" + data + "\'' }"
 
-    print(json_payload)
     # send to mattermost
-    # response = requests.post(url, json_payload)
+    response = requests.post(url, json_payload)
     
-    # if not response.status_code == 200:
-    #     print("could not post to", url)
-    #     print("Error code", response.status_code)
-    # else:
-    #     print("Successfully posted to ", url, response.status_code)
+    if not response.status_code == 200:
+        print("could not post to", url)
+        print("Error code", response.status_code)
+    else:
+        print("Successfully posted to ", url, response.status_code)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Send a message to given webhook (e.g. mattermost)')
@@ -23,8 +22,6 @@ if __name__ == "__main__":
 
     webhook_url = args.webhook
 
-    message = "CityIO is down again :roll_eyes: \n" + args.data
-
-    print(message)
+    message = "CityIO is down again :roll_eyes: \n" + " ".join(args.data)
 
     send(webhook_url, message)
