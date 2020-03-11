8cd56e9f374b94672a93486938859efd3b0a8abf
Jonas <jonas.luft@hcu-hamburg.de>
2020-03-11 14:43:29 +0100

    don't resend when noo change

--- run.sh
+++ run.sh
@@ -8,8 +8,18 @@ fi
 webhook=$1
 
 exited_containers=$(docker ps -a | grep Exited | sed "s/^.*Exited/Exited/")
-if [ -n "$exited_containers" ]; 
-then 
-    echo "found exited container!"
-    python3 send_to_mattermost.py $webhook $exited_containers
+
+if [ "$(cat lastmsg)" = "$exited_containers" ];
+then
+    exit
+else
+    rm lastmsg
+    touch lastmsg
+    echo $exited_containers >> lastmsg
+
+    if [ -n "$exited_containers" ]; 
+    then 
+        echo "found new exited container(s)!"
+        python3 send_to_mattermost.py $webhook $exited_containers
+    fi
 fi
